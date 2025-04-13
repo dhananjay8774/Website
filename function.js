0e8245b338653a8e34b53ef4de8d469261a193b5
Dhananjay Kathmore Patil <dhananjaykathmore8774@gmail.com>
2025-04-13 16:28:43 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,34 @@
+
+function greet(){
+    console.log("Welcome")
+  }
+
+  greet();function add(){
+    let a=1;
+    let b=5;
+    let total = a+b;
+    console.log(total);
+  }
+
+  function mul(a,b){
+    return a*b;
+  }
+let result = mul(2,2)
+console.log(result);
+
+
+let language = function (){
+    console.log("BKL");
+}
+language();
+
+let college=()=>{
+    console.log("parul university..");
+}
+college();
+
+let dept = (name) =>{
+
+    console.log("Department name is: ", name);
+};
+    dept ("CSE - CS");
